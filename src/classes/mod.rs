6e7856f9829ce7050ead7690fdf816cd96b50912
Bjørn Uhre Arnholtz <bjorn@bjornhub.com>
2016-12-03 23:27:08 +0100

    fix

--- src/classes/mod.rs
+++ src/classes/mod.rs
@@ -1,6 +1,11 @@
+enum Location {
+    Point(u64, u64)
+    Container()
+}
 struct Point(u64, u64);
 
-trait Object {
-    fn get_location(&self) -> Point(),
 
+
+trait Location {
+    fn get_location(&self) -> Point();
 }
